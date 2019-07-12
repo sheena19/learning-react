d99f01babbf15d120be4725f6619e28e2d745465
sheenashah <shah.sheena19@gmail.com>
2019-07-12 17:34:38 -0400

    onclick and state manipulation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+
+    state = {
+        persons: [
+            {name: 'Sheena', age: 25},
+            {name: 'Chintan', age: 24},
+            {name: 'Siddhant', age: 24}
+        ]
+    };
+
+    switchNameHandler = () => {
+        // console.log('Was clicked!');
+        this.setState({persons: })
+      this.state.persons[0].name = 'Sheena Shah';
+    };
+
     render() {
         return (
             <div className="App">
-                <p className="App-intro">
-                    <h1>Hello I am react app</h1>
-                </p>
+                <h1>Hello I am react app</h1>
+                <p>This is really working !</p>
+                <button onClick={this.switchNameHandler }>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+                    My Hobbies: Gaming
+                </Person>
             </div>
         );
+        //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello I am rendered!'));
     }
 }
 
